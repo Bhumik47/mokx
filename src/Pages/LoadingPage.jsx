31e339400261933a3e7f644e6b072879063bddaa
Bhumik <bhumikdehane@gmail.com>
2023-07-29 10:41:56 -0700

    Built Initial Loding page

---
+++ src/Pages/LoadingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function LoadingPage() {
+  return (
+      <div className='loading'>
+          <div>
+              <img src="Assets/MOkx logo 1.png" alt="" />
+
+          </div>
+          <p>Back to Vedas 🕉️</p>
+    </div>
+  )
+}
+
+export default LoadingPage
